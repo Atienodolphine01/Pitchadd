2c1c3b6124f6171734988958acb5ac7661912b71
Dolphine Atieno <awuordolphine65@gmail.com>
2020-05-05 17:28:48 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dolphine:dolphine@localhost/pitchadd'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
